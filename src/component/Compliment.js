34e4ae92ef23a977f1c7f802e0957186540d364d
Lisa Wand <lisavenneker21@gmail.com>
2020-12-17 20:02:16 -0500

    naming error overwrite

--- src/component/Compliment.js
+++ src/component/Compliment.js
@@ -4,31 +4,31 @@ import Axios from 'axios'
 const Compliment = (props) => {
   const [compliment, setCompliment] = useState([])
 
-    const getCompliment = async () => {
-      try{
-        const data = await Axios.get('https://complimentr.com/api')
-        setCompliment(data.data.compliment)
-        console.log(data.data.compliment)
-      }catch (error){
-        throw error
-      }
+  const getCompliment = async () => {
+    try {
+      const data = await Axios.get('https://complimentr.com/api')
+      setCompliment(data.data.compliment)
+      console.log(data.data.compliment)
+    } catch (error) {
+      throw error
     }
+  }
 
-    useEffect (() => {
-      getCompliment()
-    },[setCompliment])
+  useEffect(() => {
+    getCompliment()
+  }, [setCompliment])
 
-   return (
-       <div className="wrapper">
-         <h2> Welcome to Jody's! <br/>
-           Here to help you with a compliment on a bad day 
+  return (
+    <div className="wrapper">
+      <h2> Welcome to Jody's! <br />
+           Here to help you with a compliment on a bad day
          </h2>
-          <button onClick={() => getCompliment()}> Compliment me! </button>
-          <h2>
-            {compliment}
-       </h2>
-       </div>
-   )
+      <button onClick={() => getCompliment()}> Compliment me! </button>
+      <h2>
+        {compliment}
+      </h2>
+    </div>
+  )
 }
 
 export default Compliment
